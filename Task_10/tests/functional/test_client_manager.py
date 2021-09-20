e89ad016366f66deeab3ffb3ad3a5a40b4a8d486
Reynaldo Guzman <reynaldito@gmail.com>
2021-09-20 11:05:44 -0400

    updatedTestsToUseClientObject

--- Task_10/tests/functional/test_client_manager.py
+++ Task_10/tests/functional/test_client_manager.py
@@ -1,6 +1,7 @@
 import json
 import pytest
 
+from truck_delivery_rayus007.client import Client
 from truck_delivery_rayus007.client_manager import ClientManager
 
 
@@ -11,31 +12,35 @@ def client_manager(mocker):
 
 
 def test_save_document(client_manager):
-    id_test = "123"
-    object_to_save = {"123": {"a": "a"}}
-    result_save = client_manager.save_document(id_test, object_to_save)
+    id_test = "111111"
+    client_to_save = Client(111111, "Pepito1", "pepito1.perez@gmail.com", 70700000, "cbba - Av. Heroinas", 111,
+                            1111111111).to_dict()
+    result_save = client_manager.save_document(id_test, client_to_save)
     assert result_save is True
 
 
 def test_get_by_id(client_manager):
-    id_test = "123"
-    object_to_save = {"123": {"a": "a"}}
-    client_manager.save_document(id_test, object_to_save)
+    id_test = "111111"
+    client_to_save = Client(111111, "Pepito1", "pepito1.perez@gmail.com", 70700000, "cbba - Av. Heroinas", 111,
+                            1111111111).to_dict()
+    client_manager.save_document(id_test, client_to_save)
     result = client_manager.get_document(id_test)
-    assert result == object_to_save
+    assert result == client_to_save
 
 
 def test_get_all(client_manager):
-    id_test = "123"
-    object_to_save = {"123": {"a": "a"}}
-    client_manager.save_document(id_test, object_to_save)
+    id_test = "111111"
+    client_to_save = Client(111111, "Pepito1", "pepito1.perez@gmail.com", 70700000, "cbba - Av. Heroinas", 111,
+                            1111111111).to_dict()
+    client_manager.save_document(id_test, client_to_save)
     result = client_manager.get_all()
-    assert result[0] == json.dumps(object_to_save)
+    assert result[0] == json.dumps(client_to_save)
 
 
 def test_delete(client_manager):
-    id_test = "123"
-    object_to_save = {"123": {"a": "a"}}
-    client_manager.save_document(id_test, object_to_save)
+    id_test = "111111"
+    client_to_save = Client(111111, "Pepito1", "pepito1.perez@gmail.com", 70700000, "cbba - Av. Heroinas", 111,
+                            1111111111).to_dict()
+    client_manager.save_document(id_test, client_to_save)
     result = client_manager.delete(id_test)
     assert result is None
--- Task_10/tests/unit/test_client_manager.py
+++ Task_10/tests/unit/test_client_manager.py
@@ -2,6 +2,7 @@ import json
 import pytest_mock
 import pytest
 
+from truck_delivery_rayus007.client import Client
 from truck_delivery_rayus007.client_manager import ClientManager
 
 
@@ -12,43 +13,48 @@ def client_manager(mocker):
 
 
 def test_save_document_mock(mocker):
-    id_test = "123"
-    object_to_save = {"123": {"a": "a"}}
+    id_test = "111111"
+    client_to_save = Client(111111, "Pepito1", "pepito1.perez@gmail.com", 70700000, "Cbba - Av. Heroinas", 111, 1111111111).to_dict()
     cli_manager = ClientManager()
     mocker.patch("redis.Redis.set").return_value = True
-    mocker.patch("redis.Redis.get").return_value = json.dumps(object_to_save)
-    result_save = cli_manager.save_document(id_test, object_to_save)
+    mocker.patch("redis.Redis.get").return_value = json.dumps(client_to_save)
+    result_save = cli_manager.save_document(id_test, client_to_save)
     assert result_save is True
 
 
 def test_get_by_id_mock(mocker):
-    id_test = "123"
-    object_to_save = {"123": {"a": "a"}}
+    id_test = "111111"
+    client_to_save = Client(111111, "Pepito1", "pepito1.perez@gmail.com", 70700000, "Cbba - Av. Heroinas", 111,
+                            1111111111).to_dict()
     mocker.patch("redis.Redis.set").return_value = True
     mocker.patch("redis.Redis.get").return_value = json.dumps(id_test)
     cli_manager = ClientManager()
-    cli_manager.save_document(id_test, object_to_save)
-    result = cli_manager.get_document(object_to_save)
+    cli_manager.save_document(id_test, client_to_save)
+    result = cli_manager.get_document(client_to_save)
     assert str(result) == id_test
 
 
 def test_get_all_mock(mocker):
-    id_test = "123"
-    object_to_save = {"123": {"a": "a"}}
+    id_test = "111111"
+    client_to_save = Client(111111, "Pepito1", "pepito1.perez@gmail.com", 70700000, "Cbba - Av. Heroinas", 111,
+                            1111111111).to_dict()
     mocker.patch("redis.Redis.set").return_value = True
     mocker.patch("redis.Redis.get").return_value = json.dumps(id_test)
     cli_manager = ClientManager()
-    cli_manager.save_document(id_test, object_to_save)
+    cli_manager.save_document(id_test, client_to_save)
+    mocker.patch("redis.Redis.get").return_value = json.dumps(id_test)
     result = cli_manager.get_all()
-    assert result[0] == json.dumps(object_to_save)
+    assert result[0] == json.dumps(client_to_save)
 
 
 def test_delete(mocker):
-    id_test = "123"
-    object_to_save = {"123": {"a": "a"}}
+    id_test = "111111"
+    client_to_save = Client(111111, "Pepito1", "pepito1.perez@gmail.com", 70700000, "Cbba - Av. Heroinas", 111,
+                            1111111111).to_dict()
     mocker.patch("redis.Redis.set").return_value = True
-    mocker.patch("redis.Redis.get").return_value = json.dumps(object_to_save)
+    mocker.patch("redis.Redis.get").return_value = json.dumps(client_to_save)
     cli_manager = ClientManager()
-    cli_manager.save_document(id_test, object_to_save)
+    cli_manager.save_document(id_test, client_to_save)
+    mocker.patch("redis.Redis.get").return_value = None
     result = cli_manager.delete(id_test)
     assert result is None
